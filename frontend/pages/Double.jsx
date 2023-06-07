4c4fca58e2216f5a24172f4c1a1c8f56f8a6a4b2
naveen <naveen15aug1982@gmail.com>
2023-06-08 00:06:25 +0530

    redployment commit 16

--- frontend/pages/Double.jsx
+++ frontend/pages/Double.jsx
@@ -1,18 +1,13 @@
 import { useSearchParams } from "react-router-dom";
-import {  useAppBridge } from "@shopify/app-bridge-react"; 
 import { useState } from "react";
  
  
   export default function Tool() {
   let [searchParams, setSearchParams] = useSearchParams();
-  let  localOrigin ;
-  const app = useAppBridge();  
-  if (app) {     
-    localOrigin=app.localOrigin ;
-  }
-  const fetch = useAuthenticatedFetch();
+ 
+ 
   const product_id=searchParams.id;
-   const host=searchParams.host; /* */
+   const host=searchParams.host;  
   return (
     <div className="app">
      welcome to test page {product_id}
