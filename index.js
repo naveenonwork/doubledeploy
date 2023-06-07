4626767bd28d0559daf9673ceff741612a64d87b
naveen <naveen15aug1982@gmail.com>
2023-06-07 20:47:52 +0530

    redployment commit 5

--- index.js
+++ index.js
@@ -140,14 +140,14 @@ app.get("/static/js/:id/:jsfile",   async  (req, res) => {
        
      var saved_shop="https://"+settings.shop+'/';
       //res.status(200).send(saved_shop);
-       if(saved_shop==referer){
+     //  if(saved_shop==referer){
         var filePath=cwd +'/'+req.params.jsfile ;
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         const readStream = fs.createReadStream(filePath);
         readStream.pipe(res); 
-       } else{
+      /*  } else{
         res.status(500).send("OOPS!!");
-       }
+       } */
        
   
 })
